7876af0d72fdf8c00c237249f8e60f5d348f9f14
tdenkov123 <tdenkov123@mail.ru>
2024-03-30 11:51:30 +0300

    VideoRecognition class made

--- src/line_recognition.py
+++ src/line_recognition.py
@@ -4,26 +4,35 @@ import matplotlib
 from matplotlib.pyplot import imshow
 from matplotlib import pyplot as plt
 
-cap = cv2.VideoCapture(0)
+class VideoRecognition():
+    def getCameraDeviceIndexes() -> list[int]:
+        indexes = []
+        i = 10
+        while i > 0:
+            cap = cv2.VideoCapture(i)
+            if cap.read()[0]:
+                indexes.append(i)
+                cap.release()
+        return indexes
+    
 
-while True:
-    ret, frame = cap.read()
-    if ret == True:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(gray, (7, 7), 0)
-        cann = cv2.Canny(blur, 0, 85, 0)
-        lines = cv2.HoughLinesP(cann, 1, np.pi/180, 0, minLineLength=10, maxLineGap=250)
+    def LineRecognotion(i) -> None:
+        cap = cv2.VideoCapture(0)
+        while True:
+            ret, frame = cap.read()
+            if ret == True:
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                blur = cv2.GaussianBlur(gray, (7, 7), 0)
+                cann = cv2.Canny(blur, 0, 85, 0)
+                cv2.imshow("mask", cann)
+            else:   
+                print("Failed to get an image")
+            k = cv2.waitKey(60) & 0xff
+            if k == 27:
+                break
 
-        # if lines is not None:
-        #     for line in lines:
-        #         x1, y1, x2, y2 = line[0]
-        #         cv2.line(frame, (x1, y1), (x2, y2), (0, 150, 0), 3)
-        cv2.imshow("mask", cann)
-    else:
-        print("Failed to get an image")
-    k = cv2.waitKey(60) & 0xff
-    if k == 27:
-        break
+        cv2.destroyAllWindows()
+        cap.release()
 
-cv2.destroyAllWindows()
-cap.release()
+if __name__ == "__main__":
+    VideoRecognition.LineRecognotion(0)
