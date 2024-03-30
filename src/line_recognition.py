5eb7e5c2c59923fc7420460ae032c67c15c6b14a
tdenkov123 <tdenkov123@mail.ru>
2024-03-30 11:38:02 +0300

    Cannying the video

---
+++ src/line_recognition.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+import matplotlib
+from matplotlib.pyplot import imshow
+from matplotlib import pyplot as plt
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if ret == True:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray, (7, 7), 0)
+        cann = cv2.Canny(blur, 0, 85, 0)
+        lines = cv2.HoughLinesP(cann, 1, np.pi/180, 0, minLineLength=10, maxLineGap=250)
+
+        # if lines is not None:
+        #     for line in lines:
+        #         x1, y1, x2, y2 = line[0]
+        #         cv2.line(frame, (x1, y1), (x2, y2), (0, 150, 0), 3)
+        cv2.imshow("mask", cann)
+    else:
+        print("Failed to get an image")
+    k = cv2.waitKey(60) & 0xff
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
+cap.release()
