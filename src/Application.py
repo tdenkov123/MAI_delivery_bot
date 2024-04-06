70829d9762f397f51a1fc6b52a9a5ea0b24fdcb3
tdenkov123 <tdenkov123@mail.ru>
2024-04-06 10:53:58 +0300

    Application class added

---
+++ src/Application.py
@@ -0,0 +1,13 @@
+import Table
+import VideoRecognotion
+
+
+class Application:
+    def process():
+        pass
+
+    def show(table) -> None:
+        pass
+
+if __name__ == "__main__":
+    Application.process()
