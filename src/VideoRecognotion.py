181ede678279738350c3e8c56d4d6ffe41343ea7
tdenkov123 <tdenkov123@mail.ru>
2024-04-06 10:26:35 +0300

    -typo

--- src/VideoRecognotion.py
+++ src/VideoRecognotion.py
@@ -1,10 +1,7 @@
 import cv2
-import numpy as np
-import matplotlib
-from matplotlib.pyplot import imshow
-from matplotlib import pyplot as plt
+import Application
 
-class VideoRecognition():
+class VideoRecognition(Application):
     def getCameraDeviceIndexes() -> list[int]:
         indexes = []
         i = 10
@@ -16,14 +13,14 @@ class VideoRecognition():
         return indexes
     
 
-    def LineRecognotion(i) -> None:
-        cap = cv2.VideoCapture(0)
+    def LineRecognotion(i = 0) -> None:
+        cap = cv2.VideoCapture(i)
         while True:
             ret, frame = cap.read()
             if ret == True:
                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                 blur = cv2.GaussianBlur(gray, (7, 7), 0)
-                cann = cv2.Canny(blur, 0, 85, 0)
+                cann = cv2.Canny(blur, 50, 85, 0)
                 cv2.imshow("mask", cann)
             else:   
                 print("Failed to get an image")
@@ -37,4 +34,4 @@ class VideoRecognition():
 
 
 if __name__ == "__main__":
-    VideoRecognition.LineRecognotion(0)
+    VideoRecognition.LineRecognotion()
