7717667d70c2450825abc607125b046078780387
tdenkov123 <tdenkov123@mail.ru>
2024-04-13 13:22:20 +0300

    Added showContours func

--- src/VideoRecognotion.py
+++ src/VideoRecognotion.py
@@ -1,7 +1,7 @@
 import cv2
-import Application
+from cv2 import drawContours
 
-class VideoRecognition(Application):
+class VideoRecognition():
     def getCameraDeviceIndexes() -> list[int]:
         indexes = []
         i = 10
@@ -14,14 +14,22 @@ class VideoRecognition(Application):
     
 
     def LineRecognotion(i = 0) -> None:
-        cap = cv2.VideoCapture(i)
+        cap = cv2.VideoCapture(i)   
         while True:
             ret, frame = cap.read()
             if ret == True:
                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                blur = cv2.GaussianBlur(gray, (7, 7), 0)
-                cann = cv2.Canny(blur, 50, 85, 0)
-                cv2.imshow("mask", cann)
+                blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+                def showCanny():
+                    cann = cv2.Canny(blurred, 50, 85, 0)
+                    cv2.imshow("mask", cann)
+                def showContours():
+                    threshholded = cv2.threshold(blurred, 100, 255, cv2.THRESH_BINARY)[1]
+                    contours, _ = cv2.findContours(threshholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                    cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
+                    cv2.imshow("contours", frame)
+                showContours()
+                #showCanny()
             else:   
                 print("Failed to get an image")
             k = cv2.waitKey(60) & 0xff
