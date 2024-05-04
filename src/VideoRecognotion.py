4a74d05f75fdaf36df6dfabfa1864e4f43a03943
tdenkov123 <tdenkov123@mail.ru>
2024-05-04 10:40:29 +0300

    typo

--- src/VideoRecognotion.py
+++ src/VideoRecognotion.py
@@ -3,6 +3,9 @@ import numpy as np
 
 class VideoRecognition():
     def Vid2Lines(i = 0) -> None:
+        THRESH = 100
+        EPS = 0.005
+        
         cap = cv2.VideoCapture(i)
         if not cap.isOpened():
             print("Failed to open video capture")
@@ -12,7 +15,7 @@ class VideoRecognition():
             ret, frame = cap.read()
             if ret:
                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                ret,thresh_img = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+                ret,thresh_img = cv2.threshold(gray, THRESH, 255, cv2.THRESH_BINARY)
                 contours, _ = cv2.findContours(thresh_img,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
                 
                 max=0
@@ -23,8 +26,7 @@ class VideoRecognition():
                         max=countour.shape[0]
                 
                 arclen = cv2.arcLength(sel_countour, True)
-                eps = 0.0005
-                epsilon = arclen * eps
+                epsilon = arclen * EPS
                 approx = cv2.approxPolyDP(sel_countour, epsilon, True)
                 canvas = frame.copy()
                 for pt in approx:
